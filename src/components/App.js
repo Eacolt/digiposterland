e380263c9d7d2b202cf87d138bb5c718692e6e11
liuzhaoting <liuzhaoting@100tal.com>
2019-04-10 20:36:34 +0800

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,31 @@
 
 import D3demo1 from './D3demo1.js'
 import EchartDemo1 from './EchartDemo1'
-class App extends React.Component{
-	constructor(props){
+class App extends React.Component {
+	constructor(props) {
 		super(props)
 		this.state = {
-			showme:false
+			showme: false
 		}
 	}
 
-	componentDidMount(){
+	componentDidMount() {
 		let self = this;
-		setInterval(()=>{
-			self.setState(Object.assign(this.state,{
-				showme:!self.state.showme
+		setInterval(() => {
+			self.setState(Object.assign(this.state, {
+				showme: !self.state.showme
 			}))
-		},2000)
-		 
+		}, 2000)
+
 	}
-	render(){
+	render() {
 		return (
 			<div>
-		<D3demo1 style={{display:'none'}}/>
-		<EchartDemo1 shown={this.state.showme}/>
+				<D3demo1 style={{ display: 'none' }} />
+				<EchartDemo1 shown={this.state.showme} />
 			</div>
-	
-		 
+
+
 		)
 	}
 }
--- src/components/D3demo1.js
+++ src/components/D3demo1.js
@@ -1,33 +1,73 @@
 class D3demo1 extends React.Component{
     constructor(props){
         super(props)
+        this.state ={
+            svgWidth:500,
+            svgHeight:450
+        }
     }
     componentDidMount(){
+        let self = this;
         let datas = [120,200,300,100]
         let svgdemo = this.refs.svgdemo;
-        let mysvg = D3.select(svgdemo).append('svg').attr('width',500).attr('height',400);
-        mysvg
-        .attr('class','MyRect')
-        .selectAll('.MyRect')
+        let mysvg = D3.select(svgdemo).append('svg').attr('width',this.state.svgWidth).attr('height',this.state.svgHeight);
+
+        
+        let myrect = mysvg.selectAll()
             .data(datas)
             .enter()
             .append('rect')
+            .attr('class','MyRect')
             .attr('x',function(d,i){
                 return 120+35*i
             })
             .attr('y',function(d,i){
-                return 400-d
+                return self.state.svgHeight-d
             })
           
             .attr('width',30)
             .attr('height',function(d,i){
                 return d
-            })
+            });
+            console.log(myrect)
+         //let tr = D3.select(svgdemo)
+         myrect.each(function(d,i,nodes){
+             D3.select(nodes[i].parentNode).append('text').text('haha')
+             .attr('x',)
+         });
+
+         let scaleOrdinal = D3.scaleOrdinal()
+                            .domain([0,1,2,3,4,5])
+                            .range([100,150,200,250,300,350])
+
+         let linear = D3.scaleLinear()
+                        .domain([100,0])
+                        .range([0,300]);
+        console.log(linear(50),scaleOrdinal(2))
+
+        let xAxis = D3.axisBottom(scaleOrdinal);
+        let yAxis = D3.axisLeft(linear);
+        let gxAxios = mysvg.append('g')
+                           .attr('transform',`translate(0,${self.state.svgHeight-20})`)
+                           
+                            .call(xAxis);
+
+        let gyAxios = mysvg.append('g')
+        .attr('transform',`translate(100,130)`)
+        
+            .call(yAxis);
+                    
+                    
+         
+                // .selectAll('tr')
+                // .data([1,2,3,4])
+                // .enter()
+                // .append('tr')
     }
     render(){
         return (
  
-            <div ref='svgdemo'>
+            <div ref='svgdemo' className='lala'>
 
 
 
